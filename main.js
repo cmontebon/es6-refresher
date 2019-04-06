69ca83ae15470bad3e0fd4fca95dd9271ba2835e
Christian <cpmontebon25@gmail.com>
2019-04-06 21:30:50 +0800

    arrow function

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-// const & let
+// CONST & LET
+
+/**
+ * use let if you want to change its value in the future
+ * ps. u can reassign value of an object/array but u cant recreate it
+ */
+
 let name = 'Foo';
 name = 'Bar';
 
@@ -8,15 +14,28 @@ const person = {
 };
 
 person.name = 'Foo';
-console.log(person.name);
 
 const num = [1,2,3,4];
 num.push(5);
-console.log(num)
 
-// arrow functions
 
-// foreach
+// ARROW FUNCTION
+
+// this is a normal function
+function normalFunction() {
+  console.log('this is a normal function')
+}
+
+/**
+ * using arrow function
+ * u can omit the parenthesis if u have only 1 paremeter
+ * omit the curly braces if its one liner
+ * u can use const or let
+ */
+const arrowFunction = () => { console.log('this is an arrow function from es6')}
+
+
+// FOREACH
 
 // map
 
