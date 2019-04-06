1e128432697a4bd14a5959fcfb385473f7982125
Christian <cpmontebon25@gmail.com>
2019-04-06 21:17:07 +0800

    const and let

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 // const & let
+let name = 'Foo';
+name = 'Bar';
+
+const person = {
+  name: '',
+  age: 23
+};
+
+person.name = 'Foo';
+console.log(person.name);
+
+const num = [1,2,3,4];
+num.push(5);
+console.log(num)
 
 // arrow functions
 
